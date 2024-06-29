d03f1beeb15d975030f2597396eb0ee6ed4a6b1a
KUSHAGRA BANSAL <bansalkushagra150802@gmail.com>
2024-06-29 16:04:08 +0530

    Added input integer

--- util/common.ts
+++ util/common.ts
@@ -18,3 +18,23 @@ export function inputStringFromUser(prompt: string): Promise<string> {
         });
     });
 }
+
+export function inputIntegerFromUser(promptMessage: string): Promise<number> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise<number>((resolve) => {
+        rl.question(promptMessage, (inputString: string) => {
+            rl.close();
+            const userInput = parseInt(inputString.trim(), 10);
+            if (!isNaN(userInput)) {
+                resolve(userInput);
+            } else {
+                console.log("Invalid input. Please enter a valid integer.");
+                resolve(inputIntegerFromUser(promptMessage));
+            }
+        });
+    });
+}
