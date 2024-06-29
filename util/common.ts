67e61d5c0f923c49c9549d69c793e00be3515de5
KUSHAGRA BANSAL <bansalkushagra150802@gmail.com>
2024-06-29 16:02:55 +0530

    Add input string

--- util/common.ts
+++ util/common.ts
@@ -1,4 +1,20 @@
+import * as readline from 'readline';
+
 export function rollDice(): number {
     let min = 1, max = 6;
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function inputStringFromUser(prompt: string): Promise<string> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise<string>((resolve) => {
+        rl.question(prompt, (inputString: string) => {
+            rl.close();
+            resolve(inputString);
+        });
+    });
 }
